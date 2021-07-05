d9baa10b1c2844980aba770d50b1e7258f7c669a
Poojani Herath <92888@fhss.sjp.ac.lk>
2021-07-05 20:46:33 +0530

    string formatting

---
+++ stringFormatting.py
@@ -0,0 +1,12 @@
+string1 = "Cat"
+string2 = "Dog"
+
+print(string1, end='/')
+print(string2)
+
+
+string1 = "Exam is coming {}"
+print(string1.format("very soon!.."))
+
+
+
---
+++ stringFormatting2.py
@@ -0,0 +1,3 @@
+string1 = "Today is {0} and month is {1}."
+print(string1.format("Monday", "July"))
+print(string1.format("Tuesday", "August"))
