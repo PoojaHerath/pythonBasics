679adb20935842c2b66916c35f32b9ff0a4544bb
Poojani Herath <92888@fhss.sjp.ac.lk>
2021-07-05 13:41:34 +0530

    first commit

---
+++ basicMathOperators.py
@@ -0,0 +1,26 @@
+#addition
+a = 10
+b = 15
+c = a + b
+print(c)
+
+#substraction
+a = 43
+b = 22
+c = a - b
+print(c)
+
+#multipication
+print(2*5)
+
+#division
+print(64/2)
+
+#modulo operator
+print(23%2)
+
+#floor division
+print(10//3)
+
+#exponential operator
+print(2**10)
---
+++ operationswithUserInputs.py
@@ -0,0 +1,9 @@
+#input numbers
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+
+#Add two numbers
+sum = int(num1) + int(num2)
+
+#Display the sum
+print(sum)
