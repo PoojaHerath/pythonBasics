203acf05176f3e764326bf51346da5492c5aa5e3
Poojani Herath <92888@fhss.sjp.ac.lk>
2021-07-13 10:48:42 +0530

    findReplace

---
+++ findReplace.py
@@ -0,0 +1,36 @@
+string = "The surprise is here somewhere"
+num = "45-455-44555"
+
+x = string.find("surprise")
+y = num.find("5")
+
+print(x)
+print(y)
+
+print("The surprise is here somewhere".find("here"))
+print("The surprise is here somewhere".find("IS"))
+
+
+# ==================================================================
+
+
+replaceString = "The surprise is here somewhere"
+print(replaceString)
+replaceString = replaceString.replace("is ", "isn't ")
+print(replaceString)
+
+
+replaceNum = "45-455-44555"
+print(replaceNum)
+replaceNum = replaceNum.replace("5", "7")
+print(replaceNum)
+
+replaceNum = "45-455-44555"
+replaceNum = replaceNum[0:2].replace("5", "7")
+print(replaceNum)
+
+
+
+
+
+
