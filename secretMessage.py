79d842874a13063dc095075bc0d2c2acb1ad8af8
Poojani Herath <92888@fhss.sjp.ac.lk>
2021-08-23 12:39:52 +0530

    secretMsg

---
+++ secretMessage.py
@@ -0,0 +1,31 @@
+word = "Meet me outside for something special"
+word = word.replace("for", "4")
+reverseString = word[::-1]
+print(reverseString)
+
+word = word.replace("something", "SMTH")
+word = word.replace("special", "SPL")
+reverseString = word[::-1]
+print(reverseString)
+
+
+# ====================================================================
+
+
+word2 = "I am sorry, I'll catch you as soon as possible"
+word2 = word2.replace("I am sorry,", "IMS,")
+word2 = word2.replace("you", "u")
+word2 = word2.replace("as soon as possible", "asap")
+print(word2)
+
+# reverse the above one
+word2reverse = word2[::-1]
+print(word2reverse)
+
+
+# ====================================================================
+
+
+
+
+
